452245c6953cdd5e8a7b57ee2a60a1e55759963f
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-05-28 17:11:25 +0530

    w2_3
    .

--- Su19_Image_Processing/function.py
+++ Su19_Image_Processing/function.py
@@ -9,7 +9,7 @@ from numpy.linalg import inv
 from sklearn import linear_model, datasets
 
 
-#takes input image shape=(num_points,2); obj shape=(num_points,3)
+# output = 3x3 projection_matrix, input image shape=(num_points,2); obj shape=(num_points,3)
 def projection_matrix_direct(imgp1, objp):
 
     imgp = np.ones((315,3))
@@ -28,8 +28,28 @@ def projection_matrix_direct(imgp1, objp):
     
     return camera_matrix
 
+# output = 3x3 projection_matrix, input image shape=(num_points,2); obj shape=(num_points,3) 
+def projection_matrix3(img_p, obj_p):
+    
+    C = []
 
-def projection_matrix(img_p, obj_p):
+    for i in range(315):
+        C.append(np.array([obj_p[i,0], obj_p[i,1],1,0,0,0, (-1)*obj_p[i,0]*img_p[i,0], 
+                           (-1)*obj_p[i,1]*img_p[i,0], (-1)*img_p[i,0]]))
+        C.append(np.array([0,0,0, obj_p[i,0], obj_p[i,1],1, 
+                           (-1)*obj_p[i,0]*img_p[i,1], (-1)*obj_p[i,1]*img_p[i,1],(-1)*img_p[i,1]]))
+    
+    c = np.array(C)
+    ctc = np.matmul(c.T,c)
+    u, s, vh = np.linalg.svd(ctc, full_matrices=True)
+    L = vh[-1]
+    H = L.reshape(3, 3)
+    H = H/H[-1,-1]
+    
+    return H
+
+# output = 3x4 projection_matrix, input image shape=(num_points,2); obj shape=(num_points,3) 
+def projection_matrix4(img_p, obj_p):
     
     C = []
 
@@ -77,13 +97,34 @@ def return_objpoints():
 
     return objp
 
-    
 
-"""   
+def homo_img(img_p):
+    img_homo = np.ones((3,315))
+    img_homo[0,:] = img_p[:,0]
+    img_homo[1,:] = img_p[:,1]
+    
+    return img_homo
+
+def homo_obj3(obj_p):
+    obj_homo3 = np.ones((3,315))
+    obj_homo3[0,:] = obj_p[:,0]
+    obj_homo3[1,:] = obj_p[:,1]
+    
+    return obj_homo3
+
+def homo_obj4(obj_p):
+    obj_homo4 = np.ones((4,315))
+    obj_homo4[0,:] = obj_p[:,0]
+    obj_homo4[1,:] = obj_p[:,1]
+    obj_homo4[2,:] = obj_p[:,2]
+    
+    return obj_homo4    
+
+
 img_p=return_imagepoints()
 obj_p=return_objpoints()
-P=projection_matrix(img_p,obj_p)
+P=projection_matrix3(img_p,obj_p)
 
 print(P)
-"""
+
 
