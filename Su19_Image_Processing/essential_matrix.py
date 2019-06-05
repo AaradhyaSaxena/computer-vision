6b51faf979d7d803c9e940b814a17f9d1849b9ae
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-06-06 00:49:18 +0530

    w3_3

---
+++ Su19_Image_Processing/essential_matrix.py
@@ -0,0 +1,48 @@
+###################################
+######## Essential matrix  ########
+
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
+from glob import glob
+
+import math
+from scipy import linalg
+from numpy.linalg import inv
+from sklearn import linear_model, datasets
+from utils import *
+from utils_beta import *
+
+
+_3d_points=[]
+_2d_points=[]
+
+grid_x = 21
+grid_y = 15
+grid = (grid_x, grid_y)
+
+x,y=np.meshgrid(range(grid_x),range(grid_y))
+
+world_points=np.hstack((x.reshape(grid_x*grid_y,1),y.reshape(grid_x*grid_y,1),np.zeros((grid_x*grid_y,1)))).astype(np.float32)
+
+img_paths = ['opencv_frame_0.png','opencv_frame_1.png']
+# img_paths=glob('*.png') #get paths of all all images
+for path in img_paths:
+    im=cv2.imread(path)
+    ret, corners = cv2.findChessboardCorners(im, grid)
+    
+    if ret: #add points only if checkerboard was correctly detected:
+        _2d_points.append(corners) #append current 2D points
+        _3d_points.append(world_points) #3D points are always the same
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(_3d_points, _2d_points, (im.shape[1],im.shape[0]),None,None)
+
+homo_im = np.array(_2d_points)
+homo_ob = np.array(_3d_points)
+
+e = essential_matrix(homo_im,homo_ob)
+
+t = returnT_fromE(e)
+print(t)
+
+
