1586f65c3bc9ed8175930c7478add37a581414a7
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-06-06 00:24:57 +0530

    w3_1

--- Su19_Image_Processing/Notebook1_SingleCamera.py
+++ Su19_Image_Processing/Notebook1_SingleCamera.py
@@ -10,12 +10,14 @@ from sklearn import linear_model, datasets
 from function import *
 
 # cam = cv2.VideoCapture(0)
+
 # cv2.namedWindow("test")
+
+# img_counter = 0
 
 # while True:
 #     ret, frame = cam.read()
-#     cv2.imshow("test111", frame)
-    
+#     cv2.imshow("test", frame)
 #     if not ret:
 #         break
 #     k = cv2.waitKey(1)
@@ -26,14 +28,15 @@ from function import *
 #         break
 #     elif k%256 == 32:
 #         # SPACE pressed
-# #         img_name = "opencv_frame_{}.png".format(img_counter)
-#         img_name = "a2.png"
+#         img_name = "opencv_frame_{}.png".format(img_counter)
 #         cv2.imwrite(img_name, frame)
 #         print("{} written!".format(img_name))
-# #         img_counter += 1
+#         img_counter += 1
 
 # cam.release()
+
 # cv2.destroyAllWindows()
+
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((21*15,3), np.float32)
