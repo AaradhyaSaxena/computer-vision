1f8b2c5a544d7d44231c8c5bce4070f5e01336c9
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-06-10 21:00:43 +0530

    w4_1

--- Su19_Image_Processing/04_essential_matrix.py
+++ Su19_Image_Processing/04_essential_matrix.py
@@ -1,4 +1,6 @@
-# Here we calculate the value of intrinsic callibration constant of our camera
+###################################
+######## Essential matrix  ########
+###################################
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -8,9 +10,8 @@ from glob import glob
 import math
 from scipy import linalg
 from numpy.linalg import inv
-# from sklearn import linear_model, datasets
 from utils import *
-
+from utils_beta import *
 
 _3d_points=[]
 _2d_points=[]
@@ -18,41 +19,39 @@ _2d_points=[]
 grid_x = 21
 grid_y = 15
 grid = (grid_x, grid_y)
-h = 480
-w = 640
 
 x,y=np.meshgrid(range(grid_x),range(grid_y))
 
 world_points=np.hstack((x.reshape(grid_x*grid_y,1),y.reshape(grid_x*grid_y,1),np.zeros((grid_x*grid_y,1)))).astype(np.float32)
 
-# p1 = "\images"
-img_paths=glob('*.png') #get paths of all all images
+img_paths = ['opencv_frame_0.png','opencv_frame_1.png']
+# img_paths=glob('*.png') #get paths of all all images
 for path in img_paths:
     im=cv2.imread(path)
     ret, corners = cv2.findChessboardCorners(im, grid)
-
+    
     if ret: #add points only if checkerboard was correctly detected:
         _2d_points.append(corners) #append current 2D points
         _3d_points.append(world_points) #3D points are always the same
 
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(_3d_points, _2d_points, (im.shape[1],im.shape[0]),None,None)
+# print(mtx)
 
-### Undistortion
-nmtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+im = np.array(_2d_points)
+# homo_ob = np.array(_3d_points)
+# print(homo_im.shape)
 
-### Reprojection Error
-tot_error = 0
-for i in xrange(len(_3d_points)):
-    _2d_points2, _ = cv2.projectPoints(_3d_points[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv2.norm(_2d_points[i],_2d_points2, cv2.NORM_L2)/len(_2d_points2)
-    tot_error += error
-    err = tot_error/len(_3d_points)
-print "total error: ", err
+e = essential_matrix_cal(im)
+print("essential_matrix:/n",e,"/n")
 
-# np.savez('data/parameters',k_new = nmtx, k = mtx, rvecs =rvecs, tvecs = tvecs, distortion_coeff = dist, reprojection_error = err)
-# l = np.load('data/parameters.npz')
-# l.files
+t = returnT_fromE(e)
+print("translation:/n",t,"/n")
+# tx = [[0,(-1)*t[2],t[1]],[t[2],0,(-1)*t[0]],[(-1)*t[1],t[0],0]]
 
-# p = returnP_fromK(mtx,rvecs,tvecs)
-# print(p)
+r1,r2 = returnR_fromE(e)
+print("rotation_matrix",r1,"/n",r2,"/n")
+# u, v = returnUV_fromE(e)
 
+# m1,m2,m3,m4 = returnP_fromE(e)
+
+# print(m1,m2,m3,m4)
