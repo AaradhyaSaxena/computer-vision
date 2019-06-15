d7b714e4bc9726e267fded1b2baea9dd4c3138ef
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-06-16 00:10:43 +0530

    w4-6

--- Su19_Image_Processing/utils.py
+++ Su19_Image_Processing/utils.py
@@ -6,8 +6,419 @@ from glob import glob
 import math
 from scipy import linalg
 from numpy.linalg import inv
-# from sklearn import linear_model, datasets
 from numpy import linalg as LA
+
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+##### -----------Utils_gamma-----------------
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+
+def find_corner_pts(img,n_pts=500):
+    orb = cv2.ORB_create(edgeThreshold=15, patchSize=31,
+                nlevels=8, fastThreshold=20,scaleFactor=1.2, 
+                WTA_K=2, scoreType=cv2.ORB_HARRIS_SCORE, 
+                firstLevel=0,nfeatures= n_pts)
+    kp = orb.detect(img,None)
+    kp, des = orb.compute(img, kp)
+
+    corn_arr = []
+    for i in range(len(kp)):
+        corn_arr.append(kp[i].pt)
+    # corner = np.array(corn_arr)
+
+    # corner_x = corner[:,0]
+    # corner_y = corner[:,1]
+
+    # return corner_x, corner_y
+    return corn_arr
+
+
+#shape of im2=im1=(500,2) i
+def essential_matrix(im1, im2):
+
+    length = im1.shape[0]
+    homo = np.ones((2,length,3))
+    homo_im = np.ones((2,length,3))
+    # homo[:,:,0] = im1.T
+    # homo[:,:,1] = im2.T
+    homo[0,:,:2] = im1
+    homo[1,:,:2] = im2
+    
+    kk = np.load("data/parameters.npz")
+    # k = kk['k_new']
+    k = kk['k']
+
+    #shape of k=3x3 
+    homo_im[0,:,:] = np.matmul(inv(k),homo[0,:,:].T).T
+    homo_im[1,:,:] = np.matmul(inv(k),homo[1,:,:].T).T
+    
+    xa=homo_im[0,:,0]
+    xb=homo_im[1,:,0]
+    ya=homo_im[0,:,1]
+    yb=homo_im[1,:,1]
+    c=np.ones((length,))
+    A=[xb*xa,xb*ya,yb*xa,yb*ya,xb,yb,xa,ya,c] 
+    A=np.array(A).T
+
+    # A = np.hstack(((homo_im[1,:,0]*homo_im[0,:,0]).reshape((length,1)),
+    #   (homo_im[1,:,0]*homo_im[0,:,1]).reshape((length,1)),
+    #   homo_im[1,:,0].reshape((length,1)),(homo_im[1,:,1]*homo_im[0,:,0]).reshape((length,1)),
+    #   (homo_im[1,:,1]*homo_im[0,:,1]).reshape((length,1)),
+    #   homo_im[1,:,1].reshape((length,1)),homo_im[0,:,0].reshape((length,1)),
+    #   homo_im[0,:,1].reshape((length,1)),np.ones((length,1))))
+    
+    ata = np.matmul(A.T,A)
+    u, s, vh = np.linalg.svd(ata, full_matrices=True)
+    L = vh[-1]
+    H = L.reshape(3, 3)
+
+    u1, s1, vh1 = np.linalg.svd(H,full_matrices=True)
+
+    s2 = np.array([(s1[0]+s1[1])/2, (s1[0]+s1[1])/2, 0])
+    left = np.matmul(u1,np.diag(s2))
+    E = np.matmul(left,vh1)
+
+
+    return E
+
+# takes in 3x3 essential matrix
+def returnT_fromE(e):
+    ete = np.matmul(e.T,e)
+    u, s, vh = np.linalg.svd(ete, full_matrices=True)
+    v = vh.T
+
+    return v[:,-1]
+
+def returnUV_fromE(e):
+    ete = np.matmul(e.T,e)
+    u, s, vh = np.linalg.svd(ete, full_matrices=True)
+    v = vh.T
+    # v = v/v[-1]
+
+    return u, v
+
+def returnR_fromE(e):
+
+    t = returnT_fromE(e)
+    tx = [[0,(-1)*t[2],t[1]],[t[2],0,(-1)*t[0]],[(-1)*t[1],t[0],0]]
+    u, v = returnUV_fromE(e)
+    z = np.array([[0,1,0],[-1,0,0],[0,0,0]])
+    w = np.array([[0,1,0],[-1,0,0],[0,0,1]])
+    d = np.array([[1,0,0],[0,1,0],[0,0,0]])
+    r1 = np.dot(u,np.dot(w.T,v.T))
+    r2 = np.dot(u,np.dot(w,v.T))
+
+    return r1,r2    
+
+def returnR1_fromE(e):
+
+    t = returnT_fromE(e)
+    tx = [[0,(-1)*t[2],t[1]],[t[2],0,(-1)*t[0]],[(-1)*t[1],t[0],0]]
+    u, v = returnUV_fromE(e)
+    z = np.array([[0,1,0],[-1,0,0],[0,0,0]])
+    w = np.array([[0,1,0],[-1,0,0],[0,0,1]])
+    d = np.array([[1,0,0],[0,1,0],[0,0,0]])
+    r1 = np.dot(u,np.dot(w.T,v.T))
+    r2 = np.dot(u,np.dot(w,v.T))
+
+    return r1
+
+
+def return_depth_im12(im1,im2,r1,t):
+
+    length = im1.shape[0]
+    homo = np.ones((2,length,3))
+    homo_im = np.ones((2,length,3))
+    # homo[:,:,0] = im1.T
+    # homo[:,:,1] = im2.T
+    homo[0,:,:2] = im1
+    homo[1,:,:2] = im2
+
+    # kk = np.load("data/parameters.npz")
+    # # k = kk['k_new']
+    # k = kk['k']
+
+    # #shape of k=3x3 
+    # homo_im[0,:,:] = np.matmul(inv(k),homo[0,:,:].T).T
+    # homo_im[1,:,:] = np.matmul(inv(k),homo[1,:,:].T).T
+    
+    rot1 = np.matmul(np.matmul(homo_im[1],r1),homo_im[0].T)
+    trans1 = np.matmul(homo_im[1],t.reshape((3,1)))
+
+    A = np.hstack((rot1,trans1))
+    ata = np.matmul(A.T,A)
+    u, s, vh = np.linalg.svd(ata, full_matrices=True)
+    Depth = vh[-1].reshape(length+1,1)
+
+    return Depth
+
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#####-----------utils_beta-------------------
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+
+def return_pcv(img_path, corners_, obj_p):
+    objpoints = [] 
+    imgpoints = []
+    imgpoints.append(corners_)
+    objpoints.append(obj_p)
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+    rx = [[1,0,0],[0,math.cos(rvecs[0][0]),math.sin(rvecs[0][0])],[0,(-1)*math.sin(rvecs[0][0]),math.cos(rvecs[0][0])]]
+    ry = [[math.cos(rvecs[0][1]),0,math.sin(rvecs[0][1])],[0,1,0],[(-1)*math.sin(rvecs[0][1]),0,math.cos(rvecs[0][1])]]
+    rz = [[math.cos(rvecs[0][2]),math.sin(rvecs[0][2]),0],[(-1)*math.sin(rvecs[0][2]),math.cos(rvecs[0][2]),0],[0,0,1]]
+    R1 = np.matmul(rx,ry)
+    R = np.matmul(R1,rz)
+    m = np.ones((3,4))
+    m[:,:3] = R
+    m[:,[3]]= np.array(tvecs)
+    cv_homography = np.matmul(mtx,m)
+    pcv = cv_homography/cv_homography[-1,-1]
+
+    return pcv
+
+def return_fundamentalM(P1, P2):
+    A1 = P1[:,:3]
+    a1 = P1[:,3]
+
+    A2 = P2[:,:3]
+    a2 = P2[:,3]
+
+    b = np.matmul(inv(A2),a2) - np.matmul(inv(A1),a1)
+    Sb = [0,(-1)*b[2],b[1],b[2],0,(-1)*b[0],(-1)*b[1],b[0],0]
+    Sb = np.array(Sb)
+    Sb = Sb.reshape((3,3))
+
+    F = np.matmul(np.matmul(inv(A1).T , Sb) ,inv(A2))
+
+    return F
+
+# 
+def errorFundamental(F,img_pt1, img_pt2):
+    x1 = homo_img(img_pt1)
+    x2 = homo_img(img_pt2)
+
+    w = np.matmul(np.kron(x2,x1).T, F.reshape((9,1)))
+    print(w.shape)
+    w1 = np.sum(w)
+
+    return w1
+
+#im shape = (2(num_of_images),Number_Of_Corners,2(x,y))
+def essential_matrix_pt(im):
+
+    length = im.shape[1]
+    homo = np.ones((2,length,3))
+    homo_im = np.ones((2,length,3))
+    homo[:,:,:2] = im[:,:,:]
+    kk = np.load("data/parameters.npz")
+    k = kk['k_new']
+    # k = kk['k']
+    homo_im[0,:,:] = np.matmul(inv(k),homo[0,:,:].T).T
+    homo_im[1,:,:] = np.matmul(inv(k),homo[1,:,:].T).T
+    
+    A = np.hstack(((homo_im[1,:,0]*homo_im[0,:,0]).reshape((length,1)),
+        (homo_im[1,:,0]*homo_im[0,:,1]).reshape((length,1)),
+        homo_im[1,:,0].reshape((length,1)),(homo_im[1,:,1]*homo_im[0,:,0]).reshape((length,1)),
+        (homo_im[1,:,1]*homo_im[0,:,1]).reshape((length,1)),
+        homo_im[1,:,1].reshape((length,1)),homo_im[0,:,0].reshape((length,1)),
+        homo_im[0,:,1].reshape((length,1)),np.ones((length,1))))
+    
+    ata = np.matmul(A.T,A)
+    u, s, vh = np.linalg.svd(ata, full_matrices=True)
+    L = vh[-1]
+    H = L.reshape(3, 3)
+
+    u1, s1, vh1 = np.linalg.svd(H,full_matrices=True)
+
+    s2 = np.array([(s1[0]+s1[1])/2, (s1[0]+s1[1])/2, 0])
+    left = np.matmul(u1,np.diag(s2))
+    E = np.matmul(left,vh1)
+
+
+    return E
+
+
+### Im shape = (2(num_of_images),Number_Of_Corners,2(x,y))
+def essential_matrix_ignoredTh2(im):
+
+    length = im.shape[1]
+    homo = np.ones((2,length,3))
+    homo_im = np.ones((2,length,3))
+    homo[:,:,:2] = im[:,:,:]
+    kk = np.load("data/parameters.npz")
+    k = kk['k_new']
+    # k = kk['k']
+    homo_im[0,:,:] = np.matmul(inv(k),homo[0,:,:].T).T
+    homo_im[1,:,:] = np.matmul(inv(k),homo[1,:,:].T).T
+    
+    A = np.hstack(((homo_im[1,:,0]*homo_im[0,:,0]).reshape((length,1)),
+        (homo_im[1,:,0]*homo_im[0,:,1]).reshape((length,1)),
+        homo_im[1,:,0].reshape((length,1)),(homo_im[1,:,1]*homo_im[0,:,0]).reshape((length,1)),
+        (homo_im[1,:,1]*homo_im[0,:,1]).reshape((length,1)),
+        homo_im[1,:,1].reshape((length,1)),homo_im[0,:,0].reshape((length,1)),
+        homo_im[0,:,1].reshape((length,1)),np.ones((length,1))))
+    
+    ata = np.matmul(A.T,A)
+    u, s, vh = np.linalg.svd(ata, full_matrices=True)
+    L = vh[-1]
+    H = L.reshape(3, 3)
+
+    return H
+
+
+def essential_matrix_kinv_ignored(homo_im):
+
+    length = homo_im.shape[1]
+
+    A = np.hstack(((homo_im[1,:,0]*homo_im[0,:,0]).reshape((length,1)),
+        (homo_im[1,:,0]*homo_im[0,:,1]).reshape((length,1)),
+        homo_im[1,:,0].reshape((length,1)),(homo_im[1,:,1]*homo_im[0,:,0]).reshape((length,1)),
+        (homo_im[1,:,1]*homo_im[0,:,1]).reshape((length,1)),
+        homo_im[1,:,1].reshape((length,1)),homo_im[0,:,0].reshape((length,1)),
+        homo_im[0,:,1].reshape((length,1)),np.ones((length,1))))
+    
+    ata = np.matmul(A.T,A)
+    u, s, vh = np.linalg.svd(ata, full_matrices=True)
+    L = vh[-1]
+    H = L.reshape(3, 3)
+
+    return H
+
+# homo_im is numpy array
+def essential_matrix_cal(im):
+
+    length = im.shape[1]
+    homo = np.ones((2,length,1,3))
+    homo_im = np.ones((2,length,1,3))
+    homo[:,:,0,:2] = im[:,:,0,:]
+    kk = np.load("data/parameters.npz")
+    k = kk['k']
+    homo_im[0,:,0,:] = np.matmul(inv(k),homo[0,:,0,:].T).T
+    homo_im[1,:,0,:] = np.matmul(inv(k),homo[1,:,0,:].T).T
+
+    A = np.hstack(((homo_im[1,:,0,0]*homo_im[0,:,0,0]).reshape((315,1)),
+        (homo_im[1,:,0,0]*homo_im[0,:,0,1]).reshape((315,1)),
+        homo_im[1,:,0,0].reshape((315,1)),(homo_im[1,:,0,1]*homo_im[0,:,0,0]).reshape((315,1)),
+        (homo_im[1,:,0,1]*homo_im[0,:,0,1]).reshape((315,1)),
+        homo_im[1,:,0,1].reshape((315,1)),homo_im[0,:,0,0].reshape((315,1)),
+        homo_im[0,:,0,1].reshape((315,1)),np.ones((315,1))))
+    
+    ata = np.matmul(A.T,A)
+    u, s, vh = np.linalg.svd(ata, full_matrices=True)
+    L = vh[-1]
+    H = L.reshape(3, 3)
+
+    return H,ata
+
+# takes in 3x3 essential matrix
+def returnT_fromE(e):
+    ete = np.matmul(e.T,e)
+    u, s, vh = np.linalg.svd(ete, full_matrices=True)
+    v = vh.T
+
+    return v[:,-1]
+
+def returnUV_fromE(e):
+    ete = np.matmul(e.T,e)
+    u, s, vh = np.linalg.svd(ete, full_matrices=True)
+    v = vh.T
+    # v = v/v[-1]
+
+    return u, v
+
+def returnR_fromE(e):
+
+    t = returnT_fromE(e)
+    tx = [[0,(-1)*t[2],t[1]],[t[2],0,(-1)*t[0]],[(-1)*t[1],t[0],0]]
+    u, v = returnUV_fromE(e)
+    z = np.array([[0,1,0],[-1,0,0],[0,0,0]])
+    w = np.array([[0,1,0],[-1,0,0],[0,0,1]])
+    d = np.array([[1,0,0],[0,1,0],[0,0,0]])
+    r1 = np.dot(u,np.dot(w.T,v.T))
+    r2 = np.dot(u,np.dot(w,v.T))
+
+    return r1,r2    
+
+def returnP_fromE(e):
+
+    t = returnT_fromE(e)
+    tx = [[0,(-1)*t[2],t[1]],[t[2],0,(-1)*t[0]],[(-1)*t[1],t[0],0]]
+    u, v = returnUV_fromE(e)
+    z = np.array([[0,1,0],[-1,0,0],[0,0,0]])
+    w = np.array([[0,1,0],[-1,0,0],[0,0,1]])
+    d = np.array([[1,0,0],[0,1,0],[0,0,0]])
+    r1 = np.dot(u,np.dot(w.T,v.T))
+    r2 = np.dot(u,np.dot(w,v.T))
+
+    m1 = np.dot(r1,np.array([[1,0,0,t[0]],[0,1,0,t[1]],[1,0,0,t[2]]]))
+    m2 = np.dot(r1,np.array([[1,0,0,(-1)*t[0]],[0,1,0,(-1)*t[1]],[1,0,0,(-1)*t[2]]]))
+    m3 = np.dot(r2,np.array([[1,0,0,t[0]],[0,1,0,t[1]],[1,0,0,t[2]]]))
+    m4 = np.dot(r2,np.array([[1,0,0,(-1)*t[0]],[0,1,0,(-1)*t[1]],[1,0,0,(-1)*t[2]]]))
+
+    return m1, m2, m3, m4
+
+def find_corners(img):
+    orb = cv2.ORB_create(edgeThreshold=15, patchSize=31,
+                nlevels=8, fastThreshold=20,scaleFactor=1.2, 
+                WTA_K=2, scoreType=cv2.ORB_HARRIS_SCORE, 
+                firstLevel=0,nfeatures= 500)
+    kp = orb.detect(img,None)
+    kp, des = orb.compute(img, kp)
+
+    corn_arr = []
+    for i in range(len(kp)):
+        corn_arr.append(kp[i].pt)
+    # corner = np.array(corn_arr)
+
+    # corner_x = corner[:,0]
+    # corner_y = corner[:,1]
+
+    # return corner_x, corner_y
+    return corn_arr
+
+### homo_im.shape = (2,numCorners,3) and E = 3x3
+def error_essential_matrix(homo_im,E):
+    err = np.matmul(np.matmul(homo_im[1],E),homo_im[0].T)
+    return np.sum(err)/np.shape(err)[0]
+
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+##########----------utils--------------------
+#############################################
+#############################################
+#############################################
 
 
 # output = 3x3 projection_matrix, input image shape=(num_points,2); obj shape=(num_points,3)
@@ -204,3 +615,17 @@ P=projection_matrix3(img_p,obj_p)
 
 print(P)
 """
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
+#############################################
