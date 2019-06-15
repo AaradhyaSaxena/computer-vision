0fb0b439f250a3f9aa5d28b7cb3a0817c1c7529f
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-06-15 19:02:02 +0530

    w4_7

--- Su19_Image_Processing/utils_beta.py
+++ Su19_Image_Processing/utils_beta.py
@@ -178,7 +178,24 @@ def returnP_fromE(e):
 
 	return m1, m2, m3, m4
 
+def find_corners(img):
+	orb = cv2.ORB_create(edgeThreshold=15, patchSize=31,
+				nlevels=8, fastThreshold=20,scaleFactor=1.2, 
+				WTA_K=2, scoreType=cv2.ORB_HARRIS_SCORE, 
+				firstLevel=0,nfeatures= 500)
+	kp = orb.detect(img,None)
+	kp, des = orb.compute(img, kp)
 
+	corn_arr = []
+	for i in range(len(kp)):
+		corn_arr.append(kp[i].pt)
+	# corner = np.array(corn_arr)
+
+	# corner_x = corner[:,0]
+	# corner_y = corner[:,1]
+
+	# return corner_x, corner_y
+	return corn_arr
 
 
 
--- Su19_Image_Processing/utils_gamma.py
+++ Su19_Image_Processing/utils_gamma.py
@@ -7,11 +7,11 @@ import math
 from utils import *
 from utils_beta import *
 
-def find_corners(img):
+def find_corner_pts(img,n_pts=500):
 	orb = cv2.ORB_create(edgeThreshold=15, patchSize=31,
 				nlevels=8, fastThreshold=20,scaleFactor=1.2, 
 				WTA_K=2, scoreType=cv2.ORB_HARRIS_SCORE, 
-				firstLevel=0,nfeatures=500)
+				firstLevel=0,nfeatures= n_pts)
 	kp = orb.detect(img,None)
 	kp, des = orb.compute(img, kp)
 
@@ -27,25 +27,39 @@ def find_corners(img):
 	return corn_arr
 
 
+#shape of im2=im1=(500,2) i
 def essential_matrix(im1, im2):
 
 	length = im1.shape[0]
 	homo = np.ones((2,length,3))
 	homo_im = np.ones((2,length,3))
-	homo[:,:,0] = im1.T
-	homo[:,:,1] = im2.T
+	# homo[:,:,0] = im1.T
+	# homo[:,:,1] = im2.T
+	homo[0,:,:2] = im1
+	homo[1,:,:2] = im2
+	
 	kk = np.load("data/parameters.npz")
 	# k = kk['k_new']
 	k = kk['k']
+
+	#shape of k=3x3 
 	homo_im[0,:,:] = np.matmul(inv(k),homo[0,:,:].T).T
 	homo_im[1,:,:] = np.matmul(inv(k),homo[1,:,:].T).T
 	
-	A = np.hstack(((homo_im[1,:,0]*homo_im[0,:,0]).reshape((length,1)),
-		(homo_im[1,:,0]*homo_im[0,:,1]).reshape((length,1)),
-		homo_im[1,:,0].reshape((length,1)),(homo_im[1,:,1]*homo_im[0,:,0]).reshape((length,1)),
-		(homo_im[1,:,1]*homo_im[0,:,1]).reshape((length,1)),
-		homo_im[1,:,1].reshape((length,1)),homo_im[0,:,0].reshape((length,1)),
-		homo_im[0,:,1].reshape((length,1)),np.ones((length,1))))
+	xa=homo_im[0,:,0]
+	xb=homo_im[1,:,0]
+	ya=homo_im[0,:,1]
+	yb=homo_im[1,:,1]
+	c=np.ones((length,))
+	A=[xb*xa,xb*ya,yb*xa,yb*ya,xb,yb,xa,ya,c] 
+	A=np.array(A).T
+
+	# A = np.hstack(((homo_im[1,:,0]*homo_im[0,:,0]).reshape((length,1)),
+	# 	(homo_im[1,:,0]*homo_im[0,:,1]).reshape((length,1)),
+	# 	homo_im[1,:,0].reshape((length,1)),(homo_im[1,:,1]*homo_im[0,:,0]).reshape((length,1)),
+	# 	(homo_im[1,:,1]*homo_im[0,:,1]).reshape((length,1)),
+	# 	homo_im[1,:,1].reshape((length,1)),homo_im[0,:,0].reshape((length,1)),
+	# 	homo_im[0,:,1].reshape((length,1)),np.ones((length,1))))
 	
 	ata = np.matmul(A.T,A)
 	u, s, vh = np.linalg.svd(ata, full_matrices=True)
