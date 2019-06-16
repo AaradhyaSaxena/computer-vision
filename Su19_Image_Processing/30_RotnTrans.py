41eaf893cbd91fc7438efe9029adf3ec6290211b
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-06-17 00:21:22 +0530

    w5_1

--- Su19_Image_Processing/30_RotnTrans.py
+++ Su19_Image_Processing/30_RotnTrans.py
@@ -6,7 +6,7 @@ from glob import glob
 import math
 from scipy import linalg
 from numpy.linalg import inv
-from utils_gamma import *
+from utils import *
 
 _3d_points=[]
 _2d_points=[]
@@ -20,35 +20,49 @@ image=[]
 X =[]
 
 
-img_paths=glob('*.png')
-for path in img_paths:
-    img = cv2.imread(path,0)
-    corners = find_corner_pts(img,500)
-    _2d_points.append(corners)
-    image.append(img)
+# img_paths=glob('*.png')
+# for path in img_paths:
+#     Img = cv2.imread(path,0)
+#     corners = find_corner_pts(img,500)
+#     _2d_points.append(corners)
+#     image.append(img)
 
-homo_im = np.array(_2d_points)
-image = np.array(image)
+# homo_im = np.array(_2d_points)
+# image = np.array(image)
 
 
-N = len(homo_im)
+# N = len(homo_im)
 
-for i in range(N):
-	j = i+1
-	while(j<N):
-		e = essential_matrix(homo_im[i],homo_im[j])
-		essentialMatrix.append(e)
-		tvecs.append(returnT_fromE(e))
-		rvecs.append(returnR1_fromE(e))
-		depth.append(return_depth_im12(homo_im[i],homo_im[j],returnR1_fromE(e),returnT_fromE(e)))
-		index.append((i,j))
-		X.append(np.stack((image[i],image[j]),axis=-1))
-		j = j+1
+# for i in range(N):
+# 	j = i+1
+# 	while(j<N):
+# 		e = essential_matrix(homo_im[i],homo_im[j])
+# 		essentialMatrix.append(e)
+# 		tvecs.append(returnT_fromE(e))
+# 		rvecs.append(returnR1_fromE(e))
+# 		depth.append(return_depth_im12(homo_im[i],homo_im[j],returnR1_fromE(e),returnT_fromE(e)))
+# 		index.append((i,j))
+# 		X.append(np.stack((image[i],image[j]),axis=-1))
+# 		j = j+1
 
-np.savez('data/training_data', e = essentialMatrix, r = rvecs, t = tvecs, 
-			depth = depth, corners = homo_im, index = index, X = X)
-l = np.load('data/training_data.npz')
-print(l.files)
+# np.savez('data/training_data', e = essentialMatrix, r = rvecs, t = tvecs, 
+# 			depth = depth, corners = homo_im, index = index, X = X)
+# l = np.load('data/training_data.npz')
+# print(l.files)
+
+#-----------------visualize-------------------
+# for i in range(8):
+# 	img0 = l['X'][i]
+# 	img = img0[:,:,1]
+# 	cv2.imshow('img',img)
+# 	k = cv2.waitKey(0)
+# 	# wait for ESC key to exit
+# 	if k == 27:
+# 		cv2.destroyAllWindows()
+
+# cv2.destroyAllWindows()
+
+
 
 
 #----------------predicted--------------------
